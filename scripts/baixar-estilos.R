7fee5622b1167e823d6554170af306890704e3df
Luiza Fulgêncio <fulgencioluiza@gmail.com>
2019-05-30 10:59:06 -0300

    xxx

---
+++ scripts/baixar-estilos.R
@@ -0,0 +1,6 @@
+if(!require("devtools")) install.packages("devtools")
+if(!require("minimalTemplate")) devtools::install_github("kguidonimartins/minimalTemplate")
+library(devtools)
+library(minimalTemplate)
+
+minimalTemplate
